4d9ebb98f641886c29915bfa79550b7d96d04cb2
vhorozheiev <vhorozheiev@gmail.com>
2021-10-28 14:00:57 +0300

    first commit

--- app.js
+++ app.js
@@ -1,44 +1,7 @@
-class Vehicle {
-    constructor(carModel, carYear, maxSpeed) {
-        this.model = carModel;
-        this.year = carYear;
-        this.maxSpeed = maxSpeed;
-    }
-
-    displayInfo() {
-        console.log(`Name: ${this.model}; Year: ${this.year}; Max Speed: ${this.maxSpeed};`);
-    }
-}
-
-class Car extends Vehicle {
-    constructor(carModel, carYear, maxSpeed) {
-        super(carModel, carYear, maxSpeed);
-        this.type = "car";
-    }
-    transportPeople() {
-        console.log("I am starting transporting passengers");
-    }
-    static setColor(color) {
-        this.color = color;
-        console.log(`Color: ${this.color}`);
-    }
-
-    displayInfo() {
-        console.log(`Name: ${this.model}; Year: ${this.year}; Max Speed: ${this.maxSpeed}; Color: ${this.color}`);
-    }
-}
-
-class Truck extends Vehicle {
-    constructor(carModel, carYear, maxSpeed) {
-        super(carModel, carYear, maxSpeed);
-        this.type = "truck";
-    }
-
-    transportContainer() {
-        console.log("I am starting transporting heavy container");
-    }
-} 
-
+import Vehicle from './Vehicle.js';
+import Car from './Car.js';
+import Truck from './Truck.js';
+import Bike from './Bike.js';
 
 let car = new Car("Opel", 2005, 180);
 car.transportPeople();
@@ -49,3 +12,6 @@ car.displayInfo(); // override metod
 let truck = new Truck("Man", 2003, 160);
 truck.transportContainer();
 truck.displayInfo(); // call father's method
+
+let bike = new Bike("Honda", 2010, 240);
+bike.showMaxSpeed();
