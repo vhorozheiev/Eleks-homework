c153d5da44d3027f60e3121013b38515e8c329ca
vhorozheiev <vhorozheiev@gmail.com>
2021-11-01 12:45:00 +0200

    third version of solution

--- app.js
+++ app.js
@@ -4,14 +4,14 @@ import Truck from './truck.js';
 import Bike from './bike.js';
 
 let car = new Car("Opel", 2005, 180);
-car.transportPeople();
 //car.setColor("green");  // error if we call static method on object
 Car.setColor("red");  // it works if we call static method on Class
 car.displayInfo(); // override metod
+car.transportPeople(); //call method from class
 
 let truck = new Truck("Man", 2003, 160);
-truck.transportContainer();
 truck.displayInfo(); // call father's method
+truck.transportContainer(); //call method from class
 
 let bike = new Bike("Honda", 2010, 240);
 bike.showMaxSpeed(); //call method from class
