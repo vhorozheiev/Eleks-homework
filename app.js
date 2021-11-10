95363eb6da9d91fb287d12812567de27f0ddde09
vhorozheiev <vhorozheiev@gmail.com>
2021-11-10 19:06:36 +0200

    test version of solution

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 import { Chance } from "chance";
 import * as fs from "fs/promises";
-
+//instance of chance
 let chance = new Chance();
 
+//create object with some properties
 let student = {
   name: chance.first({
     nationality: "en",
@@ -13,7 +14,9 @@ let student = {
   rate: Math.round(Math.random() * 100) + 1,
 };
 
+//save string from object
 let obj = JSON.stringify(student);
+//write this string to file.txt
 fs.writeFile("student.txt", obj, (err) => {
   if (err) throw err;
   console.log("the file has been saved");
