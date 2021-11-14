254eb4c8c77681b52eb094b0f3527182d7f90e89
vhorozheiev <vhorozheiev@gmail.com>
2021-11-14 20:37:31 +0200

    final version of solution

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
+/* 1. Develop a simple calculator function (+, -, /, * operations), like calc(3, 3, “+”) should return 6 (3+3). Also, handle a cases when not supported math operation is provided or wrong data format
+2. Create a test suite file to verify if calculator function works correctly for positive scenarios
+3. Create a test suite file to verify if calculator function works correctly for negative scenarios*/
+
 function calculator(firstNum, secondNum, operation) {
   if (!isNaN(firstNum) & !isNaN(secondNum)) {
-      firstNum = Number(firstNum);
-      secondNum = Number(secondNum);
+    firstNum = Number(firstNum);
+    secondNum = Number(secondNum);
     if (operation == "+") {
       return firstNum + secondNum;
     } else if (operation == "*") {
